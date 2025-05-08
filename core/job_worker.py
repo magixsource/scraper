e7aa8a727671cdcdc48c5eaa818b3cc2cd6ad715
magixsource <magixsource.cn@gmail.com>
2025-05-08 21:23:30 +0800

    scrape_url -> link and ignore upload_to_web_api right now

--- core/job_worker.py
+++ core/job_worker.py
@@ -53,7 +53,7 @@ def get_csv_data_v2(scraped):
     output = StringIO()
     writer = csv.writer(output, quoting=csv.QUOTE_ALL, escapechar='\\')
     # 写入表头
-    header = ['scrape_uuid', 'scrape_url', 'scrape_at']
+    header = ['scrape_uuid', 'link', 'scrape_at']
     for middle_dict in scraped[0].values():
         for inner_dict in middle_dict.values():
             for element in inner_dict:
@@ -164,9 +164,10 @@ async def process_single_job_sync(job, auth_token):
         scraped = await scrape(job)
         LOG.info(f"Scraped result for url: {job.startUrl}, with result: \n{scraped}")
         file_path = save_as_csv(job, scraped)
+        LOG.info(f"Saved file to: {file_path}")
         # upload to web api
-        LOG.info("Upload to web api")
-        upload_to_web_api(file_path, job, auth_token)
+        # LOG.info("Upload to web api")
+        # upload_to_web_api(file_path, job, auth_token)
     except Exception as e:
         LOG.error(f"Exception occurred: {e}\n{traceback.print_exc()}")
 
@@ -176,7 +177,8 @@ async def process_job_sync():
     if not jobs:
         return
 
-    auth_token = login_to_service()
+    # auth_token = login_to_service()
+    auth_token = None
     # 使用 ThreadPoolExecutor 来并发执行
     with concurrent.futures.ThreadPoolExecutor(max_workers=3) as pool:
         loop = asyncio.get_event_loop()
